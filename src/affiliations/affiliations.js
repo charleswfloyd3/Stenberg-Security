39c19cc403efa9f966a0423afaf1587144723f1c
Charles Floyd <charleswfloyd3@gmail.com>
2021-05-21 07:08:23 -0600

    yuh

--- src/affiliations/affiliations.js
+++ src/affiliations/affiliations.js
@@ -14,7 +14,20 @@ const Affiliations = () => {
             <li><img src="images/google2.png" className="google"></img></li>
             <li><img src="images/npr.png" className="npr"></img></li>
             </div>
-          
+           <div className="affiliation-inner-mobile">
+               <div>
+                    <li><img src="images/cbs.png" className="cbs"></img></li>
+                    <li><img src="images/newYorkTimes.png" className="newYorkTimes"></img></li>
+
+                    <li><img src="images/forbes.png" className="forbes"></img></li>
+                </div>
+                <div>
+                    <li><img src="images/goldman.png" className="goldman"></img></li>
+
+                    <li><img src="images/google2.png" className="google"></img></li>
+                    <li><img src="images/npr.png" className="npr"></img></li>
+                </div>
+            </div>
         </div>
     )
 }
