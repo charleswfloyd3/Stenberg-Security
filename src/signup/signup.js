f9462b0dab21c6cfcf1372133208be76f4d66c82
Charles Floyd <charleswfloyd3@gmail.com>
2021-11-14 02:46:13 +0200

    changed link for node server in fetch function from local host to live site

--- src/signup/signup.js
+++ src/signup/signup.js
@@ -32,7 +32,7 @@ const Signup = () => {
             console.log(JSON.stringify(data))
             setformStatus(false)                              
              try{
-            const add = await fetch("http://localhost:5000/customer", {
+            const add = await fetch("https://herokstenberg.herokuapp.com/customer", {
               method: "POST",
               headers: {
               "Content-Type": "application/json",
